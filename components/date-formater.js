fd59d4093206ee5ae4f00f2090b023d0eae88c99
woosungchoi <solut2000@gmail.com>
2020-06-23 20:18:52 +0900

    add locale date-fns

--- components/date-formater.js
+++ components/date-formater.js
@@ -1,6 +1,7 @@
 import { parseISO, format } from 'date-fns'
+import { ko } from 'date-fns/locale'
 
 export default function DateFormater({ dateString }) {
   const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'yyyy LLLL d')}</time>
+  return <time dateTime={dateString}>{format(date, 'PPP p', { locale: ko} )}</time>
 }

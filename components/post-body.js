0b05893ed4f15284ea7eae37518b7992a56e7320
WoosungChoi <solut2000@gmail.com>
2020-06-24 10:13:41 +0900

    Update post-body.js

--- components/post-body.js
+++ components/post-body.js
@@ -1,31 +1,4 @@
 import markdownStyles from './markdown-styles.module.css'
-import React, { createRef, useLayoutEffect } from 'react';
-
-const src = 'https://utteranc.es/client.js';
-export interface IUtterancesProps {
-    repo: string;
-}
-const Utterances: React.FC<IUtterancesProps> = React.memo(({ repo }) => {
-    const containerRef = createRef<HTMLDivElement>();
-    useLayoutEffect(() => {
-        const utterances = document.createElement('script');
-        const attributes = {
-            src,
-            repo,
-            'issue-term': 'url',
-            label: 'comment',
-            theme: 'github-light',
-            crossOrigin: 'anonymous',
-            async: 'true',
-        };
-        Object.entries(attributes).forEach(([key, value]) => {
-            utterances.setAttribute(key, value);
-        });
-        containerRef.current.appendChild(utterances);
-    }, [repo]);
-    return <div ref={containerRef} />;
-});
-Utterances.displayName = 'Utterances';
 
 export default function PostBody({ content }) {
   return (
@@ -34,7 +7,6 @@ export default function PostBody({ content }) {
         className={markdownStyles['markdown']}
         dangerouslySetInnerHTML={{ __html: content }}
       />
-      <Utterances repo="woosungchoi/blog-starter" />
     </div>
   )
 }

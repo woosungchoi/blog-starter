e3b249eab1bd09457caa5eb663913d7056bea9e8
woosungchoi <solut2000@gmail.com>
2020-06-28 04:15:01 +0900

    add home title link

--- components/intro.js
+++ components/intro.js
@@ -1,11 +1,13 @@
-import { CMS_NAME } from '../lib/constants'
+import Link from 'next/link'
 
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        블로그.
-      </h1>
+      <Link href="/">
+		<h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+			블로그.
+		</h1>
+      </Link>
     </section>
   )
 }

63df11945b86815be0622da5631639cb74d5c02f
woosungchoi <solut2000@gmail.com>
2020-06-28 04:42:48 +0900

    add home title link-2

--- components/intro.js
+++ components/intro.js
@@ -3,11 +3,11 @@ import Link from 'next/link'
 export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <Link href="/">
-		<h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-			블로그.
-		</h1>
-      </Link>
+      <h1 className="text-6xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
+		<Link href="/">
+			<a className="hover:underline">블로그.</a>
+		</Link>	
+	  </h1>
     </section>
   )
 }
